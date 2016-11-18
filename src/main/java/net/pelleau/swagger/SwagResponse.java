24ec4c5280fb7a0ef355d3616ddce8cd60479126
areuzeau <adrien.reuzeau@insa-rennes.fr>
2016-11-18 22:41:39 +0100

    Adding Response class + test, adding logger default config

---
+++ src/main/java/net/pelleau/swagger/SwagResponse.java
@@ -0,0 +1,62 @@
+package net.pelleau.swagger;
+
+import com.mashape.unirest.http.HttpResponse;
+
+public class SwagResponse {
+
+	private int statusCode;
+	private String statusText;
+
+	private String header;
+	private String body;
+
+	private long responseTime;
+
+	public SwagResponse(long responseTime) {
+		this.responseTime = responseTime;
+	}
+
+	public SwagResponse(HttpResponse<?> response, long responseTime) {
+		this.responseTime = responseTime;
+		this.statusCode = response.getStatus();
+		this.statusText = response.getStatusText();
+		this.header = response.getHeaders().toString();
+		this.body = response.getBody().toString();
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusText() {
+		return statusText;
+	}
+
+	public void setStatusText(String statusText) {
+		this.statusText = statusText;
+	}
+
+	public String getHeader() {
+		return header;
+	}
+
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public long getResponseTime() {
+		return responseTime;
+	}
+}
